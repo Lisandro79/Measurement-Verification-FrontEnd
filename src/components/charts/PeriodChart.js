b7ec33c4c04b6b8bf482299f88945c63bea7937b
guidocaru <carugatiguido@gmail.com>
2022-08-04 22:14:02 -0300

    did it!!

--- src/components/charts/PeriodChart.js
+++ src/components/charts/PeriodChart.js
@@ -2,7 +2,6 @@ import React from "react";
 import * as d3 from "d3";
 
 const PeriodChart = ({ data }) => {
-
   const dimensions = {
     width: 800,
     height: 400,
@@ -15,161 +14,134 @@ const PeriodChart = ({ data }) => {
   const svgHeight = height + margin.top + margin.bottom;
 
   React.useEffect(() => {
+    //zoom function
+    let zoom = d3
+      .zoom()
+      .on("zoom", (event) => {
+        xScale
+          .domain(event.transform.rescaleX(xScale1).domain())
+          .range([0, width].map((d) => event.transform.applyX(d))); //
+
+        svg.select(".line0").attr("d", line0);
+        svg.select(".line1").attr("d", line1);  
+
+        svg.select(".x-axis").call(d3.axisBottom(xScale).tickSizeOuter(0)); 
+      })
+      .scaleExtent([1, 32]); 
+
+    //x scale
+    let xScale = d3
+      .scaleTime()
+      .domain(
+        d3.extent(data, (d) => {
+          return d.time;
+        })
+      )
+      .range([0, width]);
+
+    let xScale1 = d3
+      .scaleTime()
+      .domain(
+        d3.extent(data, (d) => {
+          return d.time;
+        })
+      )
+      .range([0, width]);
+
+    //y0 scale
+    const yScale0 = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(data, (d) => {
+          return +d.eload;
+        }),
+      ])
+      .range([height, 0]);
+
+    // //y1 scale
+    let yScale1 = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(data, (d) => {
+          return d.temp;
+        }),
+      ])
+      .range([height, 0]);
+
+    //first line (eload)
+    let line0 = d3
+      .line()
+      .x(function (d) {
+        return xScale(d.time);
+      })
+      .y(function (d) {
+        return yScale0(d.eload);
+      });
+
+    //second line (eload)
+    let line1 = d3
+      .line()
+      .x(function (d) {
+        return xScale(d.time);
+      })
+      .y(function (d) {
+        return yScale1(d.temp);
+      });
 
     //svg config
     const svgEl = d3.select(svgRef.current);
     svgEl.selectAll("*").remove();
+
     const svg = svgEl
+      .call(zoom)
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    //x and y axis
-    let x = d3.scaleTime().range([0, width]);
-    let y0 = d3.scaleLinear().range([height, 0]);
-    let y1 = d3.scaleLinear().range([height, 0]);
-
-    x.domain(
-      d3.extent(data, (d) => {
-        return d.time;
-      })
-    );
-    y0.domain([
-      0,
-      d3.max(data, (d) => {
-        return d.eload;
-      }),
-    ]);
-    y1.domain([
-      0,
-      d3.max(data, (d) => {
-        return d.temp;
-      }),
-    ]);
-
-    //x axis
+    // clippath to stop line and x-axis 'spilling over'
     svg
-      .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
+    .append("defs")
+    .append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("x", 0)
+    .attr("width", width)
+    .attr("height", height);
 
-    //y0 axis
-    svg.append("g").call(d3.axisLeft(y0));
+  // call x-axis and apply the clip from the defs
+  svg
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("clip-path", "url(#clip)") // add the clip path!
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
 
-    //y1 axis
-    svg
-      .append("g")
-      .attr("class", "axisRed")
-      .attr("transform", "translate( " + width + ", 0 )")
-      .call(d3.axisRight(y1))
+  // Call the y-axis
+  svg.append("g").attr("class", "y-axis").call(d3.axisLeft(yScale0));
 
-    //x axis title
-    svg
-      .append("text")
-      .attr("text-anchor", "end")
-      .attr("x", width - 375)
-      .attr("y", height + margin.top + 0)
-      .text("Date");
-
-    //y0 axis title
-    svg.append("text")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -margin.left + 10)
-      .attr("x", -margin.top - 85)
-      .text("Energy Load [kWh]")
-
-    svg.append("text")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-90)")
-      .attr("y", -margin.right + 910)
-      .attr("x", -margin.top - 100)
-      .text("Temperature")
-
-
-    //  first line (eload)
-    let valueLine = d3
-      .line()
-      .x((d) => {
-        return x(d.time);
-      })
-      .y((d) => {
-        return y0(d.eload);
-      });
-
-    // second line (temp)
-    let valueLine2 = d3
-      .line()
-      .x((d) => {
-        return x(d.time);
-      })
-      .y((d) => {
-        return y1(d.temp);
-      });
-
-    let keys = ["Eload", "Temperature"];
-
-    //color range
-    const color = d3
-      .scaleOrdinal()
-      .domain(keys)
-      .range(["#0C6291", "#a63446"]);
+  // Append the path, bind the data, and call the line generator
+  svg
+    .append("path")
+    .datum(data) // 10. Binds data to the line
+    .attr("class", "line0") // Assign a class for styling
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("clip-path", "url(#clip)") // add the clip path!
+    .attr("d", line0); // 11. Calls the line generator
 
     svg
-      .append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", function (d) {
-        return color(d);
-      })
-      .attr("stroke-width", 1.5)
-      .attr("d", valueLine);
+    .append("path")
+    .datum(data) // 10. Binds data to the line
+    .attr("class", "line1") // Assign a class for styling
+    .attr("fill", "none")
+    .attr("stroke", "red")
+    .attr("stroke-width", 1.5)
+    .attr("clip-path", "url(#clip)") // add the clip path!
+    .attr("d", line1); // 11. Calls the line generator
 
-    svg
-      .append("path")
-      .data([data])
-      .attr("class", "line")
-      .attr("fill", "none")
-      .attr("stroke", function (d) {
-        return color(d[1]);
-      })
-      .attr("stroke-width", 1.5)
-      .attr("d", valueLine2);
-
-    //dots
-    svg
-      .selectAll("dots")
-      .data(keys)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d, i) {
-        return 300 + i * 100;
-      })
-      .attr("cy", -23)
-      .attr("r", 6)
-      .style("fill", function (d) {
-        return color(d);
-      });
-
-    //labels
-    svg
-      .selectAll("labels")
-      .data(keys)
-      .enter()
-      .append("text")
-      .attr("x", function (d, i) {
-        return 310 + i * 100;
-      })
-      .attr("y", -23)
-      .style("fill", function (d) {
-        return color(d);
-      })
-      .text(function (d) {
-        return d;
-      })
-      .attr("text-anchor", "left")
-      .style("alignment-baseline", "middle");
-
+    
   }, [data]);
 
   return <svg ref={svgRef} width={svgWidth} height={svgHeight} />;

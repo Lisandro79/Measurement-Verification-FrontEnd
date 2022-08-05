4729763b116b30641ac870562c224827e11c5219
guidocaru <carugatiguido@gmail.com>
2022-08-05 02:05:19 -0300

    close

--- src/components/charts/ModelChart.js
+++ src/components/charts/ModelChart.js
@@ -1,30 +1,65 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
 const LineChart = ({ data }) => {
-
   const dimensions = {
     width: 800,
     height: 400,
     margin: { top: 30, right: 30, bottom: 50, left: 60 },
   };
 
-  const svgRef = React.useRef(null);
+  const svgRef = useRef(null);
   const { width, height, margin = {} } = dimensions;
   const svgWidth = width + margin.left + margin.right;
   const svgHeight = height + margin.top + margin.bottom;
 
-  React.useEffect(() => {
-    
-    //svg config
-    const svgEl = d3.select(svgRef.current);
-    svgEl.selectAll("*").remove();
-    const svg = svgEl
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+  useEffect(() => {
+    //zoom function
+    let zoom = d3
+      .zoom()
+      .on("zoom", (event) => {
+        xScale0
+          .domain(event.transform.rescaleX(xScale1).domain())
+          .range([0, width].map((d) => event.transform.applyX(d)));
 
-    //x axis
-    const x = d3
+        //option 2
+        //svg.select(".line0").attr("d", line);
+
+        d3.select(svgRef.current);
+        svgEl.selectAll("*").remove();
+
+        const svg = svgEl
+          .call(zoom)
+          .append("g")
+          .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        //option 1
+        svg
+          .selectAll(".line0")
+          .data(sumstat)
+          .join("path")
+          .attr("fill", "none")
+          .attr("stroke", function (d) {
+            return color(d[0]);
+          })
+          .attr("stroke-width", 1)
+          .attr("d", function (d) {
+            return d3
+              .line()
+              .x(function (d) {
+                return xScale0(d.datetime);
+              })
+              .y(function (d) {
+                return yScale(+d.eload);
+              })(d[1]);
+          });
+
+        svg.select(".x-axis").call(d3.axisBottom(xScale0).tickSizeOuter(0));
+      })
+      .scaleExtent([1, 32]);
+
+    //x scale
+    const xScale0 = d3
       .scaleTime()
       .domain(
         d3.extent(data, function (d) {
@@ -32,10 +67,92 @@ const LineChart = ({ data }) => {
         })
       )
       .range([0, width]);
+
+    //x scale for zoom
+    const xScale1 = d3
+      .scaleTime()
+      .domain(
+        d3.extent(data, function (d) {
+          return d.datetime;
+        })
+      )
+      .range([0, width]);
+
+    //y scale
+    const yScale = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(data, function (d) {
+          return +d.eload;
+        }),
+      ])
+      .range([height, 0]);
+
+    //sumstat and keys for legend
+    const sumstat = d3.group(data, (d) => d.condition);
+    let keys = ["Baseline", "Reporting", "Counterfactual"];
+
+    // //line
+    // let line = d3.line()
+    // .x(data => xScale0(data.datetime))
+    // .y(data => yScale(+data.eload))(data[1])
+
+    //svg config
+    const svgEl = d3.select(svgRef.current);
+    svgEl.selectAll("*").remove();
+
+    const svg = svgEl
+      .call(zoom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // clippath to stop line and x axis spilling over
+    svg
+      .append("defs")
+      .append("clipPath")
+      .attr("id", "clip")
+      .append("rect")
+      .attr("x", 0)
+      .attr("width", width)
+      .attr("height", height);
+
+    // call x-axis and apply the clip from the defs
     svg
       .append("g")
-      .attr("transform", `translate(0, ${height})`)
-      .call(d3.axisBottom(x));
+      .attr("class", "x-axis")
+      .attr("clip-path", "url(#clip)")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale0));
+
+    // call y axis
+    svg.append("g").attr("class", "y-axis").call(d3.axisLeft(yScale));
+
+    //color range
+    const color = d3
+      .scaleOrdinal()
+      .domain(keys)
+      .range(["#0c6291", "#ffa600", "#a63446"]);
+
+    svg
+      .selectAll(".line0")
+      .data(sumstat)
+      .join("path")
+      .attr("fill", "none")
+      .attr("stroke", function (d) {
+        return color(d[0]);
+      })
+      .attr("stroke-width", 1)
+      .attr("d", function (d) {
+        return d3
+          .line()
+          .x(function (d) {
+            return xScale0(d.datetime);
+          })
+          .y(function (d) {
+            return yScale(+d.eload);
+          })(d[1]);
+      });
 
     //x axis title
     svg
@@ -45,57 +162,14 @@ const LineChart = ({ data }) => {
       .attr("y", height + margin.top + 20)
       .text("Date");
 
-    //y axis
-    const y = d3
-      .scaleLinear()
-      .domain([
-        0,
-        d3.max(data, function (d) {
-          return d.eload;
-        }),
-      ])
-      .range([height, 0]);
-
-    svg.append("g").call(d3.axisLeft(y));
-
     //y axis title
-    svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("transform", "rotate(-90)")
-    .attr("y", -margin.left + 10)
-    .attr("x", -margin.top - 85)
-    .text("Energy Load [kWh]")
-
-    //sumstat and keys for legend
-    const sumstat = d3.group(data, (d) => d.condition);
-    let keys = ["Baseline", "Reporting", "Counterfactual"];
-
-    //color range
-    const color = d3
-      .scaleOrdinal()
-      .domain(keys)
-      .range(["#0c6291", "#ffa600", "#a63446"]);
-
-    //add the lines
     svg
-      .selectAll(".line")
-      .data(sumstat)
-      .join("path")
-      .attr("fill", "none")
-      .attr("stroke", function (d) {
-        return color(d[1]);
-      })
-      .attr("stroke-width", 1)
-      .attr("d", function (d) {
-        return d3
-          .line()
-          .x(function (d) {
-            return x(d.datetime);
-          })
-          .y(function (d) {
-            return y(d.eload);
-          })(d[1]);
-      });
+      .append("text")
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margin.left + 10)
+      .attr("x", -margin.top - 85)
+      .text("Energy Load [kWh]");
 
     //legends
 
--- src/components/charts/PeriodChart.js
+++ src/components/charts/PeriodChart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
 const PeriodChart = ({ data }) => {
@@ -8,29 +8,29 @@ const PeriodChart = ({ data }) => {
     margin: { top: 50, right: 60, bottom: 50, left: 60 },
   };
 
-  const svgRef = React.useRef(null);
+  const svgRef = useRef(null);
   const { width, height, margin = {} } = dimensions;
   const svgWidth = width + margin.left + margin.right;
   const svgHeight = height + margin.top + margin.bottom;
 
-  React.useEffect(() => {
+  useEffect(() => {
     //zoom function
     let zoom = d3
       .zoom()
       .on("zoom", (event) => {
-        xScale
+        xScale0
           .domain(event.transform.rescaleX(xScale1).domain())
-          .range([0, width].map((d) => event.transform.applyX(d))); //
+          .range([0, width].map((d) => event.transform.applyX(d))); 
 
         svg.select(".line0").attr("d", line0);
-        svg.select(".line1").attr("d", line1);  
+        svg.select(".line1").attr("d", line1);
 
-        svg.select(".x-axis").call(d3.axisBottom(xScale).tickSizeOuter(0)); 
+        svg.select(".x-axis").call(d3.axisBottom(xScale0).tickSizeOuter(0));
       })
-      .scaleExtent([1, 32]); 
+      .scaleExtent([1, 32]);
 
     //x scale
-    let xScale = d3
+    let xScale0 = d3
       .scaleTime()
       .domain(
         d3.extent(data, (d) => {
@@ -39,6 +39,7 @@ const PeriodChart = ({ data }) => {
       )
       .range([0, width]);
 
+    //x scale for zoom
     let xScale1 = d3
       .scaleTime()
       .domain(
@@ -74,17 +75,17 @@ const PeriodChart = ({ data }) => {
     let line0 = d3
       .line()
       .x(function (d) {
-        return xScale(d.time);
+        return xScale0(d.time);
       })
       .y(function (d) {
         return yScale0(d.eload);
       });
 
-    //second line (eload)
+    //second line (temp)
     let line1 = d3
       .line()
       .x(function (d) {
-        return xScale(d.time);
+        return xScale0(d.time);
       })
       .y(function (d) {
         return yScale1(d.temp);
@@ -99,49 +100,99 @@ const PeriodChart = ({ data }) => {
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    // clippath to stop line and x-axis 'spilling over'
+    // clippath to stop line and x axis spilling over
     svg
-    .append("defs")
-    .append("clipPath")
-    .attr("id", "clip")
-    .append("rect")
-    .attr("x", 0)
-    .attr("width", width)
-    .attr("height", height);
+      .append("defs")
+      .append("clipPath")
+      .attr("id", "clip")
+      .append("rect")
+      .attr("x", 0)
+      .attr("width", width)
+      .attr("height", height);
 
-  // call x-axis and apply the clip from the defs
-  svg
-    .append("g")
-    .attr("class", "x-axis")
-    .attr("clip-path", "url(#clip)") // add the clip path!
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+    // call x-axis and apply the clip from the defs
+    svg
+      .append("g")
+      .attr("class", "x-axis")
+      .attr("clip-path", "url(#clip)") 
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale0));
 
-  // Call the y-axis
-  svg.append("g").attr("class", "y-axis").call(d3.axisLeft(yScale0));
+    // call y0 axis
+    svg.append("g").attr("class", "y-axis").call(d3.axisLeft(yScale0));
 
-  // Append the path, bind the data, and call the line generator
-  svg
-    .append("path")
-    .datum(data) // 10. Binds data to the line
-    .attr("class", "line0") // Assign a class for styling
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("clip-path", "url(#clip)") // add the clip path!
-    .attr("d", line0); // 11. Calls the line generator
+    //call y1 axis
+    svg
+      .append("g")
+      .attr("transform", "translate( " + width + ", 0 )")
+      .call(d3.axisRight(yScale1))
+
+    let keys = ["Eload", "Temperature"];
+
+    //color range
+    const color = d3
+      .scaleOrdinal()
+      .domain(keys)
+      .range(["#0C6291", "#a63446"]);
+
+    // Append the path, bind the data, and call the line generator
+    svg
+      .append("path")
+      .datum(data) 
+      .attr("class", "line0") 
+      .attr("fill", "none")
+      .attr("stroke", function (d) {
+        return color(d);
+      })
+      .attr("stroke-width", 1.5)
+      .attr("clip-path", "url(#clip)")
+      .attr("d", line0); 
 
     svg
-    .append("path")
-    .datum(data) // 10. Binds data to the line
-    .attr("class", "line1") // Assign a class for styling
-    .attr("fill", "none")
-    .attr("stroke", "red")
-    .attr("stroke-width", 1.5)
-    .attr("clip-path", "url(#clip)") // add the clip path!
-    .attr("d", line1); // 11. Calls the line generator
+      .append("path")
+      .datum(data) 
+      .attr("class", "line1") 
+      .attr("fill", "none")
+      .attr("stroke", function (d) {
+        return color(d[1]);
+      })
+      .attr("stroke-width", 1.5)
+      .attr("clip-path", "url(#clip)")
+      .attr("d", line1); 
 
-    
+    //legend dots
+    svg
+      .selectAll("dots")
+      .data(keys)
+      .enter()
+      .append("circle")
+      .attr("cx", function (d, i) {
+        return 300 + i * 100;
+      })
+      .attr("cy", -23)
+      .attr("r", 6)
+      .style("fill", function (d) {
+        return color(d);
+      });
+
+    //legend labels
+    svg
+      .selectAll("labels")
+      .data(keys)
+      .enter()
+      .append("text")
+      .attr("x", function (d, i) {
+        return 310 + i * 100;
+      })
+      .attr("y", -23)
+      .style("fill", function (d) {
+        return color(d);
+      })
+      .text(function (d) {
+        return d;
+      })
+      .attr("text-anchor", "left")
+      .style("alignment-baseline", "middle");
   }, [data]);
 
   return <svg ref={svgRef} width={svgWidth} height={svgHeight} />;

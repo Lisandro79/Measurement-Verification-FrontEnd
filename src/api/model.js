aaca1152cf289d38707bb978218ee6f3b51d419b
guidocaru <carugatiguido@gmail.com>
2022-08-10 17:23:50 -0300

    env log

--- src/api/model.js
+++ src/api/model.js
@@ -2,7 +2,7 @@ import client from "./axios";
 
 export const model = async (data) =>
   client
-    .post("/model", data, {
+    .post("/model", data, console.log(process.env.REACT_APP_API_URL), {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",

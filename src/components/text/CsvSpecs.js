1e9bcf59ad8c0f1b18bba3df517e818257c9ae0e
guidocaru <carugatiguido@gmail.com>
2022-08-17 22:03:41 -0300

    fixes

--- src/components/text/CsvSpecs.js
+++ src/components/text/CsvSpecs.js
@@ -3,7 +3,7 @@ const CsvSpecs = () => {
     <div>
     <p>{"Please upload a .csv file with the following format:"}</p>
     <ul>
-        <li>{"Column names: datetime | eload | temp"}</li>
+        <li>{"Column names: time | eload | temp"}</li>
         <li>{"Data format: Dates format ('M-D-Y H:M') | float | float"}</li>
         <li>{"Time, eload and temp columns must have the same length"}</li>
     </ul>

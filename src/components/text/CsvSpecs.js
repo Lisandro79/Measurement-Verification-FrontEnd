554c0ee555182d637a0b22aa1404bdf1dfb4fbad
guidocaru <carugatiguido@gmail.com>
2022-08-23 14:58:41 -0300

    refactor

--- src/components/text/CsvSpecs.js
+++ src/components/text/CsvSpecs.js
@@ -6,7 +6,7 @@ const CsvSpecs = () => {
         <li>{"Column names: time | eload | temp"}</li>
         <li>{"Data format: Dates format ('M/D/Y H:M') | float | float"}</li>
         <li>{"Time, eload and temp columns must have the same length"}</li>
-        <li>{"There cannot be NaN, null or empty values in the csv"}</li>
+        <li>{"There cannot be NaN, null, empty values or hours with missing values in the csv"}</li>
     </ul>
     </div>
   );
